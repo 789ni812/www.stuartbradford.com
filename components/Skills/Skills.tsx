5eac8701ba9edfc4e197f835aa76c3274ded9ec4
789ni812 <Bradfordstuart@gmail.com>
2023-07-05 12:36:42 +0100

    Improved markup semantics, created wrapper card component working on background

---
+++ components/Skills/Skills.tsx
@@ -0,0 +1,41 @@
+const Skills = () => {
+    return (
+        <>
+
+            <div className="grid lg:mb-0 lg:grid-cols-2 lg:text-left">
+                <h3 className="mb-3 text-1xl font-semibold">Technical:</h3>
+                <ul className="mb-3  text-sm">
+                    <li>Full Stack Development</li>
+                    <li>React JS</li>
+                    <li>JavaScript</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>AWS</li>
+                    <li>Firebase</li>
+                    <li>Node JS</li>
+                    <li>NGINX</li>
+                    <li>Apache</li>
+                    <li>Linux / Windows</li>
+                    <li>Drupal / WordPress</li>
+                </ul>
+                {/* <p>Currently studying Unity and C# for VR development</p> */}
+            </div>
+
+            <div className="grid lg:mb-0 lg:grid-cols-2 lg:text-left">
+                <h3 className="mb-3 text-1xl font-semibold">Project Management:</h3>
+                <ul className="mb-3 text-sm">
+                    <li>Agile Methodologies</li>
+                    <li>Certified Scrum Master</li>
+                    <li>Kanban / Scrum / Jira</li>
+                    <li>Requirements Gathering</li>
+                    <li>Resource Allocation</li>
+                    <li>Budget and Timeline Control</li>
+                    <li>Team Hiring</li>
+                </ul>
+            </div>
+        </>
+
+    )
+}
+
+export default Skills;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,49 @@
 import ProfessionalSummary from '@/components/ProfessionalSummary/ProfessionalSummary'
+import Skills from '@/components/Skills/Skills'
 import WorkHistory from './workHistory'
 import ContactInfo from '@/components/ContactInfo/ContactInfo'
-
+import ContentCard from '../components/ContentCard/ContentCard'
 
 
 export default function Home() {
     return (
-        <main
-        className={`bg-gradient-to-br from-green-800 to-gray-800 via-transparent  h-screen  min-h-screen flex-col  justify-between px-16 pt-2`}            
-        >
+        <>
+            <main
+                // className={`h-screen  min-h-screen`}
+            >
+                {/* <div className="bg-gradient-to-b bg-opacity-10 from-green-400 to-black via-transparent flex-col  justify-between px-16 pt-2"> */}
+                {/* <div className="bg-gradient-to-bl from-green-400 to-green-200 shadow-lg flex-col  justify-between px-16 pt-2"> */}
+                <div className="bg-gradient-to-bl from-green-400 to-black via-transparent flex-col  justify-between px-16 pt-2">
+                    <h1 className="lg:text-2xl lg:text-center text-2xl text-gray-300 mb-3 mx-3 text-right">
+                        stuartbradford.com
+                    </h1>
 
+                    <header className="text-right">
+                        <ContactInfo />
+                    </header>
 
-
-            <h1 className="lg:text-2xl lg:text-center text-2xl font-bold text-gray-300 mb-3 ">
-                stuartbradford.com
-            </h1>
-            <ContactInfo />
-            <div className="mb-16">
-
-                <ProfessionalSummary />
-
-            </div>
-
-            <div className="">
-                <div
-                    className="rounded-lg border border-transparent px-5 mb-16 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30"
-                >
-                    <h2 className={`mb-3 text-3xl font-semibold`}>
-                        Employment history{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <WorkHistory />
+                    <article>
+                        <ContentCard title="Professional Summary">
+                            <ProfessionalSummary />
+                        </ContentCard>
+                    </article>
+                    <article>
+                        <ContentCard title="Skills">
+                            <Skills />
+                        </ ContentCard>
+                    </article>
+                    <article>
+                        <ContentCard title="Employment History">
+                            <WorkHistory />
+                        </ ContentCard>
+                    </article>
                 </div>
-            </div>
 
 
-
-            <ContactInfo />
-
-
-
-        </main >
+            </main >
+            <footer className="bg-gradient-to-br to-green-800 from-black via-transparent pb-32">
+                <ContactInfo />
+            </footer>
+        </>
     )
 }
