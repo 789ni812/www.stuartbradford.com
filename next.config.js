15c78d2391386067e63620954dd69227cfc7416d
789ni812 <Bradfordstuart@gmail.com>
2023-07-07 11:29:34 +0100

    update build path so its compatible with both localhost and github-pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  //Add basePath
-  basePath: '/github-pages',
-}
-
-module.exports = nextConfig
+    reactStrictMode: true,
+  
+    // Set basePath depending on the environment (Github pages requires /github-pages, but local development doesn't)
+    basePath: process.env.NODE_ENV === 'production' ? '/github-pages' : '',
+  }
+  
+  module.exports = nextConfig
+  
