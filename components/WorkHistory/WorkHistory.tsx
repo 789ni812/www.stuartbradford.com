336f948b2a7cfd42efda9dbf84f8f98bd1be3496
789ni812 <Bradfordstuart@gmail.com>
2023-07-07 11:21:58 +0100

    Final proof read content adjustments

--- components/WorkHistory/WorkHistory.tsx
+++ components/WorkHistory/WorkHistory.tsx
@@ -17,7 +17,7 @@ const experiences = [
         description: [
             "As the first full-time developer at MyMynd, I undertook the critical task of migrating the original platform from an external US agency to an in-house solution, thereby reducing dependencies and enhancing operational efficiency.",
             "Conceptualised, rebuilt, and maintained the comprehensive assessment platform and wellness centre, resulting in improved user engagement and satisfaction.",
-            "As the team scaled, I transitioned into specializing in front-end development, keeping a strong emphasis on web accessibility, ensuring the design and usability cater to a diverse set of users.",
+            "As the team scaled, I transitioned into specialiing in front-end development, keeping a strong emphasis on web accessibility, ensuring the design and usability cater to a diverse set of users.",
             "Implemented data analytics and business intelligence by creating multiple assets through Google Analytics, Mixpanel and QuickSight to provide actionable insights to the business.",
         ],
         clientsWorkedWith: [
@@ -25,7 +25,7 @@ const experiences = [
             "University of Portsmouth",
         ],
         technologies: [
-            "Javascript",
+            "JavaScript",
             "React",
             "NextJS",
             "TypeScript",
@@ -35,17 +35,17 @@ const experiences = [
         ]
     },
     {
-        jobTitle: "Full Stack Development Refinement and Advancement (Feb 2020 - Feb 2021)",
+        jobTitle: "Full Stack Development Refinement and Advancement",
         icon: <AcademicCapIcon className="h-6 w-6 text-white" aria-hidden="true" />,
         duration: {
             start: "Feb 2020",
             end: "Feb 2021"
         },
         description: [
-            "I dedicated the subsequent year to studying and resharpening my skills with particular attention given to ReactJS and its ecosystem such as NextJS, TypeScript, GraphQL, and AWS.",
+            "I dedicated the subsequent year to studying and resharpening my skills with particular attention given to React and its ecosystem such as NextJS, TypeScript, GraphQL, and AWS.",
         ],
         technologies: [
-            "Javascript",
+            "JavaScript",
             "React",
             "NextJS",
             "TypeScript",
@@ -71,7 +71,7 @@ const experiences = [
         },
         description: [
             "Orchestrated the smooth running of multiple client projects, fostering robust partnerships with clients, stakeholders and developers to ensure agile projects and budgets stay on track",
-            "Concurrently served as lead developer on smaller projects, effectively mitigating resource bottlenecks and ensuring timely project completion",
+            "Concurrently served as lead developer on smaller projects and prototypes mainly focused on front-end development, effectively mitigating resource bottlenecks and ensuring timely project completion",
             "Planning and management of development projects such as e-commerce, banking and content-heavy platforms (often using Adobe Experience Manager)",
             "Acted as the pivotal Technical Project Manager for Adobe, a testament to Ensemble being one of Adobe's preferred development providers.",
             "Gathered requirements, defined scopes, allocated resources and established schedules meeting or exceeding project demands",
@@ -85,11 +85,10 @@ const experiences = [
             "Adobe",
         ],
         technologies: [
-            "Adobe Experience Manager",
-            "Java",
             "HTML",
             "CSS",
             "JavaScript",
+            "React",
         ]
     },
     {
@@ -105,7 +104,7 @@ const experiences = [
             "At the forefront of expanding the project management function for one of the \"UK's fastest-growing startups in the self-tracking and healthcare space\" as of 2015. My role was instrumental in its growth trajectory",
             "Held pivotal accountability for the effective management of multi-disciplinary teams, tasked with customising and delivering software-as-a-service, adhering to high-quality and timely delivery",
             "Leveraged Agile methodologies to orchestrate successful continuous deployments across diverse platforms including Web, Android, and iOS, ensuring a consistent user experience and seamless functionality",
-            "Simultaneously assumed the role of lead developer for smaller projects and prototypes, contributing to minimizing resource bottlenecks and fostering an environment of efficiency and productivity"
+            "Simultaneously assumed the role of lead developer for smaller projects and prototypes, focused mainly on the front-end specialising in responsive and web accessible platforms. Contributing to minimising resource bottlenecks and fostering an environment of efficiency and productivity"
         ],
         technologies: [
             "JavaScript",
@@ -131,10 +130,11 @@ const experiences = [
         },
         description: [
             "Instrumental in building a proficient web development team from the ground up, instilling agile methodologies within the team and newly designated Product Owners to foster an efficient, iterative development process",
-            "Spearheaded the migration of approximately 50 websites to Drupal 7, optimizing their performance and providing a more secure and efficient platform for growth",
+            "Spearheaded the migration of approximately 50 websites to Drupal 7, optimising their performance and providing a more secure and efficient platform for growth",
             "Enhanced subscription and e-commerce platforms, fostering improved user interactions, increased sales, and customer satisfaction",
             "In close collaboration with stakeholders, marketing, and developers, ensured the successful execution and rollout of a multitude of digital projects",
-            "Implemented innovative measures to significantly improve user experience, advanced SEO techniques, and editor environments, resulting in improved website ranking, user engagement, and ease of content management"
+            "Implemented innovative measures to significantly improve user experience, advanced SEO techniques, and editor environments, resulting in improved website ranking, user engagement, and ease of content management",
+            "A hands on role that involved the development of new websites, features, and functionality, as well as the maintenance of existing websites. I also managed the hosting infrastructure and the development team."
         ],
         technologies: [
             "Drupal 7",
@@ -161,7 +161,8 @@ const experiences = [
             "Spearheaded the recruitment and training of a new, high-performing development, design, and marketing team based in Prague",
             "Worked closely with CEO and VP of marketing developers, designers, copywriters and marketing specialists to shape a centre of excellence for AVG's global web presence",
             "Ensured the smooth operation and management of AVG's online global presence",
-            "Accountable for the successful rebrand and continuous rollout AVG's global web platforms"
+            "Accountable for the successful rebrand and continuous rollout AVG's global web platforms",
+            "Managed the development of AVG's global web presence and front-end team, specialising in the (front-end) development of new websites, features, and functionality, as well as the maintenance of existing websites and portals",
         ]
     },
     {
