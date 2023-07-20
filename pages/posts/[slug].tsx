badda688b07bdbceea87aab99386bccaa560090d
789ni812 <Bradfordstuart@gmail.com>
2023-07-20 13:53:52 +0100

    basic markdown and jsx rendering working for a post

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,61 @@
+import { GetStaticProps, GetStaticPaths } from "next";
+import { getFileBySlug, getFiles } from "../../lib/mdx";
+import { MDXRemoteSerializeResult } from "next-mdx-remote/dist/types";
+import { MDXRemote } from "next-mdx-remote";
+import matter from "gray-matter";
+
+interface PostProps {
+  post: {
+    slug: string;
+    data: {
+      title: string;
+      date: string;
+    };
+    content: MDXRemoteSerializeResult<Record<string, unknown>>;
+  };
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = await getFiles();
+
+  return {
+    paths: posts.map((p) => ({
+      params: {
+        slug: p.replace(/\.mdx/, ""),
+      },
+    })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<PostProps> = async ({ params }) => {
+    if (!params || typeof params.slug !== "string") {
+      return {
+        notFound: true,
+      };
+    }
+  
+    const post = await getFileBySlug(params.slug);
+  
+    return {
+      props: {
+        post: {
+          ...post,
+          data: {
+            ...post.data,
+            date: post.data.date.toISOString(),
+          },
+        },
+      },
+    };
+  };
+
+export default function Post({ post }: PostProps) {
+  return (
+    <div>
+      <h1>{post.data.title}</h1>
+      <p>{post.data.date}</p>
+      <MDXRemote {...post.content} />
+    </div>
+  );
+}
