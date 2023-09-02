328da9dfec661bb3d17c2bb1d8bea82ea71b1315
Stuart Bradford <5435563+789ni812@users.noreply.github.com>
2023-09-02 14:51:46 +0100

    Prepperson4sbprod (#5)
    * moved to pnpm package manager
    
    * enabled menu in header
    
    * fixed hydration issue
    
    * misc
    
    * renewed header
    
    * add project to homepage
    
    * refactored projectList
    
    * fixed projectlist hover
    
    * fixed projectlist hover
    
    * upate copy

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
-import { Card } from '@/components/Card'
+
 import { Container } from '@/components/Container'
 import {
-  GitHubIcon,
-  LinkedInIcon,
+    GitHubIcon,
+    LinkedInIcon,
 } from '@/components/SocialIcons'
 import code from '@/images/photos/code.png'
 import vRDesign from '@/images/photos/vr_design.png'
@@ -20,192 +20,164 @@ import WorkHistory from '@/components/Resume/WorkHistory'
 import Skills from '@/components/Skills/Skills'
 
 
-function MailIcon(props) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
+import ProjectList from '@/components/ProjectList'
+
+
 
 function BriefcaseIcon(props) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 9.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
+    return (
+        <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            aria-hidden="true"
+            {...props}
+        >
+            <path
+                d="M2.75 9.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
+                className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
+            />
+            <path
+                d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5"
+                className="stroke-zinc-400 dark:stroke-zinc-500"
+            />
+        </svg>
+    )
 }
 
-function ArrowDownIcon(props) {
-  return (
-    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
-      <path
-        d="M4.75 8.75 8 12.25m0 0 3.25-3.5M8 12.25v-8.5"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  )
+function ProjectIcon(props) {
+    return (
+        <svg 
+         viewBox="0 0 576 512"
+         fill="none"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            aria-hidden="true"
+            {...props}><path d="M0 80C0 53.5 21.5 32 48 32h96c26.5 0 48 21.5 48 48V96H384V80c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48H432c-26.5 0-48-21.5-48-48V160H192v16c0 1.7-.1 3.4-.3 5L272 288h96c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48H272c-26.5 0-48-21.5-48-48V336c0-1.7 .1-3.4 .3-5L144 224H48c-26.5 0-48-21.5-48-48V80z"  className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"/></svg>
+    )
 }
 
-// function Article({ article }) {
-//   return (
-//     <Card as="article">
-//       <Card.Title href={`/articles/${article.slug}`}>
-//         {article.title}
-//       </Card.Title>
-//       <Card.Eyebrow as="time" dateTime={article.date} decorate>
-//         {formatDate(article.date)}
-//       </Card.Eyebrow>
-//       <Card.Description>{article.description}</Card.Description>
-//       <Card.Cta>Read article</Card.Cta>
-//     </Card>
-//   )
-// }
+
+
+
 
 function SocialLink({ icon: Icon, ...props }) {
-  return (
-    <Link className="group -m-1 p-1" {...props}>
-      <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
-    </Link>
-  )
+    return (
+        <Link className="group -m-1 p-1" {...props}>
+            <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+        </Link>
+    )
 }
 
 function Photos() {
-  let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
+    let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
 
-  return (
-    <div className="mt-16 sm:mt-20">
-      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[vRDesign, code, taiChiPractice, projectBoost, tShirt1].map((image, imageIndex) => (
-          <div
-            key={image.src}
-            className={clsx(
-              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
-              rotations[imageIndex % rotations.length]
-            )}
-          >
-            <Image
-              src={image}
-              alt=""
-              sizes="(min-width: 640px) 18rem, 11rem"
-              className="absolute inset-0 h-full w-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+    return (
+        <div className="mt-16 sm:mt-20">
+            <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+                {[vRDesign, code, taiChiPractice, projectBoost, tShirt1].map((image, imageIndex) => (
+                    <div
+                        key={image.src}
+                        className={clsx(
+                            'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
+                            rotations[imageIndex % rotations.length]
+                        )}
+                    >
+                        <Image
+                            src={image}
+                            alt=""
+                            sizes="(min-width: 640px) 18rem, 11rem"
+                            className="absolute inset-0 h-full w-full object-cover"
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
+
+
 
 export default function Home({ articles }) {
-  return (
-    <>
+
+
+    return (
+        <>
+            <Head>
+                <title>
+                    Stuart Bradford -  Versatile Full Stack Developer, Technical Project Manager and VR Enthusiast
+                </title>
+                <meta
+                    name="description"
+                    content="I'm Stuart, Proficient and accomplished Full Stack Developer with over two decades of experience in the technology sector. Specialises in development and delivery of digital projects for Web, Android, IOS, and Virtual Reality. Excels in software engineering, project management, and VR development, having worked in diverse sectors such as Startups, Government, Health, and E-commerce. Has established a strong track record of migrating platforms in-house, enhancing operational efficiency, and improving user engagement."
+                />
+            </Head>
+            
+            
+
+
+            <Container className="mt-9">
+                <div className="max-w-2xl">
+                    <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                        Versatile Full Stack Developer, Technical Project Manager
+                    </h1>
+                    <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">I&apos;m Stuart, a Full Stack Developer with over two decades of experience in the technology sector. Specialising in development and delivery of digital projects for Web, mobile. </p>
+
+
+                    <p className="text-base text-zinc-600 dark:text-zinc-400">
+                        Worked in diverse sectors such as Startups, Government, Health, and E-commerce.
+                    </p>
+                    <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+I&apos;ve seen a lot over the years, but there&apos;s always more to learn. Alongside my day job, I&apos;m currently studying VR & AR development.</p>
+
+                    
 
 
 
-  
-      <Head>
-        <title>
-          Stuart Bradford -  Versatile Full Stack Developer, Technical Project Manager and VR Enthusiast
-        </title>
-        <meta
-          name="description"
-          content="I'm Stuart, Proficient and accomplished Full Stack Developer with over two decades of experience in the technology sector. Specialises in development and delivery of digital projects for Web, Android, IOS, and Virtual Reality. Excels in software engineering, project management, and VR development, having worked in diverse sectors such as Startups, Government, Health, and E-commerce. Has established a strong track record of migrating platforms in-house, enhancing operational efficiency, and improving user engagement."
-         />
-      </Head>
-      <Container className="mt-9">
-        <div className="max-w-2xl">
-          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Versatile Full Stack Developer, Technical Project Manager
-          </h1>
-          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">I&apos;m Stuart, a Full Stack Developer with over two decades of experience in the technology sector. Specialising in development and delivery of digital projects for Web, Android, IOS, and Virtual Reality. Excels in software engineering, project management, having worked in diverse sectors such as Startups, Government, Health, and E-commerce. Has consistently demonstrated effective project management abilities, coupled with a strong focus on full stack development, these initiatives have consistently improved user engagement and overall project success.
-          </p>
-          <div className="mt-6 flex gap-6">
-            <SocialLink
-              href="https://github.com/789ni812"
-              aria-label="Follow on GitHub"
-              icon={GitHubIcon}
-            />
-            <SocialLink
-              href="https://www.linkedin.com/in/stuart-bradford/"
-              aria-label="Follow on LinkedIn"
-              icon={LinkedInIcon}
-            />
-          </div>
-        </div>
 
-      </Container>
-      <Photos />
-      <Container className="mt-24 md:mt-28">
-      
-      <Skills />
-      </Container>
-      {/* <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-        
-          <div className="flex flex-col gap-16">
-            {articles.map((article) => (
-              <Article key={article.slug} article={article} />
-            ))}
-          </div>
-            <Resume />
-        
-        </div>
-      </Container> */}
-      <Container className="mt-24 md:mt-28">
-      <h2 className="flex text-lg font-semibold text-zinc-900 dark:text-zinc-100">
-        <BriefcaseIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3 mb-6">Work</span>
-      </h2>
-      <WorkHistory />
-      </Container>
-   
-    </>
-  )
+
+                    <div className="mt-6 flex gap-6">
+                 
+                        <SocialLink
+                            href="https://github.com/789ni812"
+                            aria-label="Follow on GitHub"
+                            icon={GitHubIcon}
+                            target="_blank"
+                        />
+                        <SocialLink
+                            href="https://www.linkedin.com/in/stuart-bradford/"
+                            target="_blank"
+                            aria-label="Follow on LinkedIn"
+                            icon={LinkedInIcon}
+                        />
+                    </div>
+                </div>
+
+            </Container>
+            <Photos />
+            <Container className="mt-24 md:mt-28">
+                <Skills />
+            </Container>
+            <Container className="mt-24 md:mt-28">
+            <h2 className="flex text-lg font-semibold text-zinc-900 dark:text-zinc-100 mb-12">
+                    <ProjectIcon className="h-6 w-6 flex-none" />
+                    <span className="ml-3 mb-6">Projects I&apos;m currently working on</span>
+                </h2>
+            <ProjectList listType="short" />
+            </Container>
+
+
+            <Container className="mt-24 md:mt-28">
+                <h2 className="flex text-lg font-semibold text-zinc-900 dark:text-zinc-100">
+                    <BriefcaseIcon className="h-6 w-6 flex-none" />
+                    <span className="ml-3 mb-6">Work</span>
+                </h2>
+                <WorkHistory />
+            </Container>
+
+        </>
+    )
 }
-
-// export async function getStaticProps() {
-//   if (process.env.NODE_ENV === 'production') {
-//     await generateRssFeed()
-//   }
-
-//   return {
-//     props: {
-//       articles: (await getAllArticles())
-//         .slice(0, 4)
-//         .map(({ component, ...meta }) => meta),
-//     },
-//   }
-// }
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,65 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import taiChiPracticeImage from '@/images/projects/taichi-expert-studying-with-books.avif'
-import projectBoostImage from '@/images/projects/GameProjectBoost.png'
-import {faUnity} from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faReact } from '@fortawesome/free-brands-svg-icons'
-import { faHtml5 } from '@fortawesome/free-brands-svg-icons'
-import { faCss3 } from '@fortawesome/free-brands-svg-icons'
-import { faNode } from '@fortawesome/free-brands-svg-icons'
-import { faCode } from '@fortawesome/free-solid-svg-icons'
-import { faShopify } from '@fortawesome/free-brands-svg-icons'
-import { faFile } from '@fortawesome/free-solid-svg-icons'
-import { faFileCode } from '@fortawesome/free-solid-svg-icons'
-import { faChartGantt } from '@fortawesome/free-solid-svg-icons'
-import { faFileCircleCheck } from '@fortawesome/free-solid-svg-icons'
-import { faFileLines } from '@fortawesome/free-solid-svg-icons'
-
-
-const projects = [
-  {
-    name: 'Tai Chi Practice',
-    description:
-      "A website about Tai Chi, using NextJS, Shopify, SanityCMS.",
-    technologies: [
-        { name: "NodeJS", icon: faNode },
-        { name: "React", icon: faReact },
-        { name: "NextJS", icon: faCode },
-        { name: "GraphQL", icon: faChartGantt },
-        {name: "TypeScript", icon: faFileCircleCheck},
-        { name: "Shopify", icon: faShopify },
-        { name: "SanityCMS", icon: faFile },
-        { name: "TailwindCSS", icon: faFileCode},
-        { name: "CSS3", icon: faCss3 },
-        { name: "HTML5", icon: faHtml5 }
-    ],
-    link: { href: 'http://taichipractice.zone', label: 'taichipractice.zone' },
-    imageLogo: taiChiPracticeImage
-  },
-  {
-    name: 'Project Boost',
-    description:
-        "To help in learning and better understand how to build games and applications in Virtual Reality I first need to get an understanding of 3D game development. So I recently completed a Unity course on just that. As part of this training course I built a game about flying a rocket through a series of obstacles, using Unity and C#. I've added a couple of tweaks to it and exported it to the web using WebGL. it's very basic, but really helped me understand the basics of 3D game development and has helped me greatly in my VR development practice.",
-    technologies: [{ name: "Unity", icon: faUnity },{ name: "C#", icon: faFileLines }],
-    link: { href: '/public/games/ProjectBoost/index.html', label: 'Project Boost' },
-    imageLogo: projectBoostImage
-  },
-]
-
-function LinkIcon(props) {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path
-        d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z"
-        fill="currentColor"
-      />
-    </svg>
-  )
-}
-
+import ProjectList from '@/components/ProjectList'
 
 
 export default function Projects() {
@@ -77,48 +18,7 @@ export default function Projects() {
         title="A couple of projects I'm working on."
         intro="I'm always working on something, Here are a few of the things I'm working on right now."
       >
-        <ul
-          role="list"
-          className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
-        >
-          {projects.map((project) => (
-            <Card as="li" key={project.name}>
-
-
-
-
-
-              <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-                <Image
-                  src={project.imageLogo}
-                  alt=""
-                  className="h-8 w-8"
-                  unoptimized
-                />
-              </div>
-              <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-                <Card.Link href={project.link.href} target_blank>{project.name}</Card.Link>
-              </h2>
-              <Card.Description>
-                <p className="pt-4">{project.description}</p>
-                <ul role="list" className="divide-y divide-gray-200">
-                    {project.technologies.map((item) => (
-                        <li key={item.name} className="py-4 flex items-center">
-                        <FontAwesomeIcon icon={item.icon} className="h-6 w-6 mr-2" />
-                        <span className="ml-3">{item.name}</span>
-                    </li>
-                    ))}
-                </ul>
-              </Card.Description>
-              
-              
-              <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-                <LinkIcon className="h-6 w-6 flex-none" />
-                <span className="ml-2">{project.link.label}</span>
-              </p>
-            </Card>
-          ))}
-        </ul>
+     <ProjectList />
       </SimpleLayout>
 
     </>
