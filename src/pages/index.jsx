6eff823dbb7cb91326829c003be44a7ac02f8149
789ni812 <Bradfordstuart@gmail.com>
2023-09-05 15:18:36 +0100

    fixed title colors

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -161,19 +161,19 @@ I&apos;ve seen a lot over the years, but there&apos;s always more to learn. Alon
                 </div>
             </Container>
             <Container className="mt-24 md:mt-28">
-            <h2 className="flex text-lg font-semibold text-zinc-900 dark:text-zinc-100 mb-12 text-teal-700">
+            <h2 className="flex text-lg font-semibold dark:text-zinc-100 mb-12 text-teal-700">
                     <ProjectIcon className="h-6 w-6 flex-none" />
                     <span className="ml-3 mb-6">Projects I&apos;m currently working on</span>
                 </h2>
 
-                <div className="grid grid-cols-1 gap-5  md:grid-cols-2 xl:grid-cols-3">
+                <div className="grid grid-cols-1 gap-5 lg:gap-32 p-8  md:grid-cols-2 xl:grid-cols-3">
             <ProjectList listType="short" />
             </div>
             </Container>
 
 
             <Container className="mt-24 md:mt-28">
-                <h2 className="flex text-lg font-semibold text-zinc-900 dark:text-zinc-100">
+                <h2 className="flex text-lg font-semibold text-teal-700 dark:text-zinc-100">
                     <BriefcaseIcon className="h-6 w-6 flex-none" />
                     <span className="ml-3 mb-6">Work</span>
                 </h2>
